972b51e60a0dd1d2fd1606567553333b2689dd89
Les Aker <me@lesaker.org>
2018-03-16 22:19:47 -0400

    support GetConfigFromEnv

--- s3/main.go
+++ s3/main.go
@@ -1,11 +1,14 @@
 package s3
 
 import (
+	"fmt"
 	"io/ioutil"
+	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"gopkg.in/yaml.v2"
 )
 
 // Client returns an S3 client
@@ -29,4 +32,27 @@ func GetObject(bucket, key string) ([]byte, error) {
 		return []byte{}, err
 	}
 	return ioutil.ReadAll(obj.Body)
+}
+
+// GetConfig loads a config struct from an S3 object
+func GetConfig(bucket, key string, opts interface{}) error {
+	obj, err := GetObject(bucket, key)
+	if err != nil {
+		return err
+	}
+	return yaml.Unmarshal(obj, opts)
+}
+
+// GetConfigFromEnv loads a config struct using bucket/object names from the environment
+func GetConfigFromEnv(opts interface{}) error {
+	bucket := os.Getenv("S3_BUCKET")
+	key := os.Getenv("S3_KEY")
+	if bucket == "" {
+		return fmt.Errorf("bucket not provided")
+	}
+	if key == "" {
+		return fmt.Errorf("s3 key not provided")
+	}
+
+	return GetConfig(bucket, key, opts)
 }
