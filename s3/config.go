56dc38b8454f56d01681858ad97e22eb4e4fa5c6
Les Aker <me@lesaker.org>
2018-06-17 13:07:23 -0500

    first wave of redoing config

--- s3/config.go
+++ s3/config.go
@@ -7,25 +7,55 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-// GetConfig loads a config struct from an S3 object
-func GetConfig(bucket, key string, opts interface{}) error {
-	obj, err := GetObject(bucket, key)
+// S3Config wraps a config from an S4 object
+type S3Config struct {
+	Bucket string
+	Key    string
+	Config interface{}
+}
+
+func (s *S3Config) Load() error {
+	if s.Bucket == "" {
+		return fmt.Errorf("bucket not provided")
+	}
+	if s.Key == "" {
+		return fmt.Errorf("s3 key not provided")
+	}
+	obj, err := GetObject(s.Bucket, s.Key)
 	if err != nil {
 		return err
 	}
-	return yaml.Unmarshal(obj, opts)
+	return yaml.Unmarshal(obj, s.Config)
+}
+
+// GetConfig loads a config struct from an S3 object
+func GetConfig(bucket, key string, config interface{}) (*S3Config, error) {
+	s := &S3Config{
+		Bucket: bucket,
+		Key:    key,
+		Config: config,
+	}
+	err := s.Load()
+	return s, err
 }
 
 // GetConfigFromEnv loads a config struct using bucket/object names from the environment
-func GetConfigFromEnv(opts interface{}) error {
+func GetConfigFromEnv(config interface{}) (*S3Config, error) {
 	bucket := os.Getenv("S3_BUCKET")
 	key := os.Getenv("S3_KEY")
-	if bucket == "" {
-		return fmt.Errorf("bucket not provided")
-	}
-	if key == "" {
-		return fmt.Errorf("s3 key not provided")
-	}
+	return GetConfig(bucket, key, config)
+}
 
-	return GetConfig(bucket, key, opts)
+// Autoreload enables reloading every $interval seconds
+func (s *S3Config) Autoreload(delay int) {
+	go func(s *S3Config, delay int) {
+		last = time.Now()
+		for {
+			// TODO: Check if I need to try to update
+			if err := s.Load(); err != nil {
+				last = time.Now()
+			}
+			time.Sleep(1)
+		}
+	}(s, delay)
 }
