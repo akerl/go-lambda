441eb5a85c12f40953e854fb802baa49d3d93f0d
Les Aker <me@lesaker.org>
2018-09-28 08:19:42 -0400

    add helper methods

--- mux/main.go
+++ mux/main.go
@@ -2,6 +2,8 @@ package mux
 
 import (
 	"github.com/akerl/go-lambda/apigw/events"
+
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
 // Receiver defines the format of an object that can process events.Requests
@@ -20,6 +22,11 @@ func NewReceiver(cf CheckFunc, af HandleFunc, hf HandleFunc, ef ErrorFunc) Recei
 		HandleFunc: hf,
 		ErrorFunc:  ef,
 	}
+}
+
+// Start launches a lambda handler from the Receiver
+func Start(r Receiver) {
+	lambda.Start(r.Handle)
 }
 
 // CheckFunc takes a events.Request and returns if the receiver can handle on it
@@ -68,4 +75,19 @@ func (rs *receiverStruct) Error(req events.Request, err error) (events.Response,
 		return rs.ErrorFunc(req, err)
 	}
 	return events.Fail("Server Error")
+}
+
+// NoCheck is an empty CheckFunc
+func NoCheck(_ events.Request) bool {
+	return true
+}
+
+// NoAuth is an empty AuthFunc
+func NoAuth(_ events.Request) (events.Response, error) {
+	return events.Response{}, nil
+}
+
+// NoError is an empty ErrorFunc
+func NoError(_ events.Request, err error) (events.Response, error) {
+	return events.Response{}, err
 }
